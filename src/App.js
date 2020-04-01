13da8a05e69aef7fe7e13d4c567e870c7decca57
Loc Fuho <loc.truong@ubitec.com>
2020-04-01 21:01:05 +0700

    set state with react hook useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       { name: 'Loc', age: 25 },
       { name: 'Trang', age: 29 }
-    ]
+    ],
+    otherState: 'other state'
+  });
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: 'Loc', age: 25 },
+        { name: 'Trang', age: 27 }
+      ]
+    })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello world</h1>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name}>Fuho</Person>
-        <Person name={this.state.persons[1].name}>Fuho</Person>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name}>Fuho</Person>
+      <Person name={personsState.persons[1].name}>Fuho</Person>
+    </div>
+  );
 }
 
-export default App;
+
+
+
+export default app;
