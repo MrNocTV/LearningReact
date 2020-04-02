0ea6fd7c9567be7f4f976f215ad99785892c58a4
Loc Fuho <loc.truong@ubitec.com>
2020-04-02 15:29:35 +0700

    Add implementation of class-based component for reference

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
-import React, { useState } from 'react';
-import './App.css';
-import Person from './Person/Person'; 
+import React, { Component } from "react";
+import Person from './Person/Person';
 
-const app = props => {
-  const [ personsState, setPersonsState ] = useState({
-    persons: [
-      { name: 'Loc', age: 25 },
-      { name: 'Trang', age: 29 }
-    ],
-    otherState: 'other state'
-  });
+class App extends Component {
 
-  const switchNameHandler = (newName) => {
-    setPersonsState({
-      persons: [
-        { name: newName, age: Math.floor(Math.random() * 30) },
-        { name: 'Trang', age: 27 }
-      ],
-      otherState: personsState.otherState
-    })
-  }
+    state = {
+        persons: [
+            { name: 'Loc', age: 25 },
+            { name: 'Trang', age: 29 }
+            ],
+        otherState: 'other state'
+    }
 
-  const nameChangeHandler = (event) => {
-    setPersonsState({
-      persons: [
-        { name: event.target.value, age: Math.floor(Math.random() * 30) },
-        { name: 'Trang', age: 27 }
-      ],
-      otherState: personsState.otherState
-    })
-  }
+    switchNameHandler = (newName) => {
+        this.setState({
+            persons: [
+                { name: newName, age: 25 },
+                { name: 'Trang', age: 29 }
+            ]
+        });
+    }
 
-  const [otherState, setOtherState] = useState('some other value');
+    changeNameHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: event.target.value, age: Math.floor(Math.random() * 30)},
+                {name: 'Trang', age: 27}
+            ]
+        });
+    }
 
-  console.log(personsState, otherState);
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello world</h1>
+                <button onClick={this.switchNameHandler.bind(this, 'FUCK')}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} change={this.changeNameHandler}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Fuho</Person>
+            </div>
+        );
+    }
 
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <button onClick={switchNameHandler.bind(this, 'FUCK')}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} click={switchNameHandler} change={nameChangeHandler}>Fuho</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>Fuho</Person>
-    </div>
-  );
 }
 
-export default app;
+export default App;
---
+++ src/AppFunctional.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person'; 
+
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'Loc', age: 25 },
+      { name: 'Trang', age: 29 }
+    ],
+    otherState: 'other state'
+  });
+
+  const switchNameHandler = (newName) => {
+    setPersonsState({
+      persons: [
+        { name: newName, age: Math.floor(Math.random() * 30) },
+        { name: 'Trang', age: 27 }
+      ],
+      otherState: personsState.otherState
+    })
+  }
+
+  const nameChangeHandler = (event) => {
+    setPersonsState({
+      persons: [
+        { name: event.target.value, age: Math.floor(Math.random() * 30) },
+        { name: 'Trang', age: 27 }
+      ],
+      otherState: personsState.otherState
+    })
+  }
+
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <button style={style} onClick={switchNameHandler.bind(this, 'FUCK')}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} click={switchNameHandler} change={nameChangeHandler}>Fuho</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>Fuho</Person>
+    </div>
+  );
+}
+
+export default app;
