6876dc96645f00475bbd34be28425106d328dbd5
Loc Fuho <loc.truong@ubitec.com>
2020-04-07 20:21:28 +0700

    hoc

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Person from '../Person/Person';
 
 const persons = (props) => {
-    console.log('[Persons.js] rendering...');
     return props.persons.map((person, index) => {
         return <Person
                 key={index}
